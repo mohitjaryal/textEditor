dc32aa07f908e3c67c0e538c739e1faa0daf0764
Mohit <mohitt.tech@gmail.com>
2025-12-07 20:46:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-# Text Editor using Pyhton
-
 import tkinter as tk
 from tkinter import filedialog, messagebox
 
-# for making new file
 def new_file():
     text.delete(1.0, tk.END)
 
-# for opening file
 def open_file():
-    file_path = filedialog.askopenfilename (defaultextension='.txt',filetypes=[('Text Files','*.txt')])
+    file_path = filedialog.askopenfilename(
+        defaultextension='.txt',
+        filetypes=[('Text Files', '*.txt')]
+    )
     if file_path:
-        with open(file_path,'r'):
+        with open(file_path, 'r') as file:
+            content = file.read()
             text.delete(1.0, tk.END)
-            text.insert(tk.END, file.read())
+            text.insert(tk.END, content)
 
-# for save file
 def save_file():
-    file_path = filedialog.askopenfilename(defaultextension='.txt',filetypes=[('Text Files','*.txt')])
+    file_path = filedialog.asksaveasfilename(
+        defaultextension='.txt',
+        filetypes=[('Text Files', '*.txt')]
+    )
     if file_path:
-        with open(file_path,'w') as file:
-            file.write(text.get(1.0,tk.END))
-            messagebox.showinfo('Info', 'File saved Successfully')
-
+        with open(file_path, 'w') as file:
+            file.write(text.get(1.0, tk.END))
+        messagebox.showinfo('Info', 'File saved successfully!')
 
 root = tk.Tk()
 root.title('VrtX')
@@ -30,13 +31,13 @@ root.geometry('800x600')
 
 menu = tk.Menu(root)
 root.config(menu=menu)
-file_menu = tk.Menu(menu)
+
+file_menu = tk.Menu(menu, tearoff=0)
 menu.add_cascade(label='File', menu=file_menu)
 file_menu.add_command(label='New', command=new_file)
 file_menu.add_command(label='Open', command=open_file)
 file_menu.add_command(label='Save', command=save_file)
 file_menu.add_separator()
-
 file_menu.add_command(label='Exit', command=root.quit)
 
 text = tk.Text(root, wrap=tk.WORD, font=('Helvetica', 12), fg='black')
