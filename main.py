8d026f773dacdcb348372c73bac03caf3e6ef4b2
Mohit <mohitt.tech@gmail.com>
2025-12-07 20:46:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ file_menu.add_command(label='Save', command=save_file)
 file_menu.add_separator()
 file_menu.add_command(label='Exit', command=root.quit)
 
-text = tk.Text(root, wrap=tk.WORD, font=('Helvetica', 12), fg='black')
+text = tk.Text(root, wrap=tk.WORD, font=('Helvetica', 12), fg='white')
 text.pack(expand=tk.YES, fill=tk.BOTH)
 
 root.mainloop()
